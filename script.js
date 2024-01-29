b385eea0385d0a535f62936bf3f489bfcc3a22f8
darkness3898 <alvinkaws@gmail.com>
2024-01-29 17:29:48 +0800

    added flip coin function

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ var rollDice = function () {
   return randomDiceRoll;
 };
 
+var flipCoin = function () {
+  var randomNumber = Math.random() * 2;
+  var randomInteger = Math.floor(randomNumber);
+  if (randomInteger == 1) {
+    return "heads";
+  } else {
+    return "tails";
+  }
+};
+
 var main = function (input) {
   var myOutputValue = "hello world";
   return myOutputValue;

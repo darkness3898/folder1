b93228fa8433b77e7f0c22b00aac4cee46c160ca
darkness3898 <alvinkaws@gmail.com>
2024-01-29 17:35:15 +0800

    reverted to roll dice function

--- script.js
+++ script.js
@@ -5,16 +5,6 @@ var rollDice = function () {
   return randomDiceRoll;
 };
 
-var flipCoin = function () {
-  var randomNumber = Math.random() * 2;
-  var randomInteger = Math.floor(randomNumber);
-  if (randomInteger == 1) {
-    return "heads";
-  } else {
-    return "tails";
-  }
-};
-
 var main = function (input) {
   var myOutputValue = "hello world";
   return myOutputValue;

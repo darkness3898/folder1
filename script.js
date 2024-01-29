e7f4e0f3543e8bee76b5a79bca4067f730f2ba39
darkness3898 <alvinkaws@gmail.com>
2024-01-29 17:27:31 +0800

    added a roll dice function

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+var rollDice = function () {
+  var randomNumber = Math.random() * 6;
+  var randomInteger = Math.floor(randomNumber);
+  var randomDiceRoll = randomInteger + 1;
+  return randomDiceRoll;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
